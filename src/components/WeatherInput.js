45f1952c56bc5a2dabe6faf172eb84084e9f2287
Abhishek Rathaur <113031809+abhishekrathaur80@users.noreply.github.com>
2024-01-19 12:58:54 +0530

    Update WeatherInput.js

--- src/components/WeatherInput.js
+++ src/components/WeatherInput.js
@@ -5,10 +5,10 @@ import { FaLocationDot } from "react-icons/fa6";
 
 import classes from "./WeatherInput.module.css";
 
-const WeatherInput = ({ onFetchData }) => {
+const WeatherInput = ({ onFetchData ,onForeCastData  }) => {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState({});
-
+  const [foreCastData, setForeCastData] = useState([]);
   const [hasCityInput, setHasCityInput] = useState(false);
   const [loading, setLoading] = useState(false);
   const apiKey = "3ba6f2e607a4063cfdf4f1966d2df482";
@@ -25,11 +25,20 @@ const WeatherInput = ({ onFetchData }) => {
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
       );
       const data = await response.data;
+
+      const { lat, lon } = data.coord;
+
+      const foreCastResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+      );
       setWeatherData(data);
-      setLoading(false);
+      setForeCastData(foreCastResponse.data);
+
       onFetchData(weatherData);
+      onForeCastData(foreCastData);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
